3a5455676bcd742c3767f959a275f3669841dc4e
zhHnan <1368214010@qq.com>
2025-04-27 10:43:06 +0800

    build: 更新 Dockerfile 并添加新服务构建
    - 更新 ms-user/Dockerfile，使用 alpine:latest 镜像并调整配置文件路径
    - 新增 ms-api/Dockerfile 和 ms-project/Dockerfile，用于构建 Docker 镜像
    - 修改 build.bat脚本，增加 ms-project 服务的构建

--- serve/build.bat
+++ serve/build.bat
@@ -20,6 +20,7 @@ SET GOOS=windows
 SET GOARCH=amd64
 go build -o ms-user/target/ms-user.exe ms-user/main.go
 go build -o ms-api/target/ms-api.exe ms-api/main.go
+go build -o ms-api/target/ms-project.exe ms-project/main.go
 :build_linux_amd64
 echo 编译Linux版本64位
 SET CGO_ENABLED=0
@@ -27,3 +28,4 @@ SET GOOS=linux
 SET GOARCH=amd64
 go build -o ms-user/target/ms-user ms-user/main.go
 go build -o ms-api/target/ms-api ms-api/main.go
+go build -o ms-api/target/ms-project ms-project/main.go
